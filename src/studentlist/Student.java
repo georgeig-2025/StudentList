fbbbe71dabf7aed40f3f9cc6bfd3b8617048ec02
Christian George-Igbinidu <georgeig@sheridancollege.ca>
2025-01-15 14:11:05 -0500

    Initial commit.

---
+++ src/studentlist/Student.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author fagun at 12/05/2024
+ */
+public class Student {
+    private String name;
+    public int studentID;
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+   
+}
